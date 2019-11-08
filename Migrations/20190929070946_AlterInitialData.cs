727c1bf1094d67197c8935760abb75f34dadd612
Kuro091 <lcminhhp@gmail.com>
2019-11-09 00:52:07 +0700

    Initial

---
+++ Migrations/20190929070946_AlterInitialData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AlterInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 3, "email@email.com", "Mark2" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 3, 2, "email@email.com", "Mark3" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ Models/ModelBuilderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+               new Employee
+               {
+                   Id = 1,
+                   Name = "Mark",
+                   Department = Dept.IT,
+                   Email = "email@email.com"
+               },
+               new Employee
+               {
+                   Id = 2,
+                   Name = "Mark2",
+                   Department = Dept.Payroll,
+                   Email = "email@email.com"
+               },
+               new Employee
+               {
+                   Id = 3,
+                   Name = "Mark3",
+                   Department = Dept.IT,
+                   Email = "email@email.com"
+               }
+
+               );
+        }
+
+
+    }
+}
